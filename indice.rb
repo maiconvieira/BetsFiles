bda78a7896ab801978eb1721f2b9688f3f6a68ed
Maicon Vieira <maiconrodrigo.vieira@gmail.com>
2021-05-21 10:48:31 -0300

    .

--- indice.rb
+++ indice.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 
 File.foreach("01_indice") do |line|
   d = line.split(',')
-  archive = d[0] + d[1] + d[2] + "-" + d[3] + "-" + d[4] + "-" + d[5] + d[6] + ".txt"
+  archive = '/archive/' + d[0] + d[1] + d[2] + '-' + d[3] + '-' + d[4] + '-' + d[5] + d[6]
   link =  "https://greyhoundbet.racingpost.com/#result-meeting/track_id=" + d[3] + "&r_date=" + d[0] + "-" + d[1] + "-" + d[2] + "&r_time=" + d[5] + ":" + d[5]
 
   File.write(archive, link, mode: "a")
